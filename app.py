9840114d051cc29dbf95cc10771700e4dce3ec25
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-22 19:26:04 +0300

    Update app.py
    Update usage scenario

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
+from collections import defaultdict
 from tkinter import Tk
 from dooby.core import init_db
-from dooby.gui.main_page import DooByApp
+from dooby.gui.main_page import DooByApp, UILayoutConfig
 from models import Partner
 
+#Инициализируем БД
 engine = init_db("partners.db")
 
+
+#Инициализация контекста модели
+ui_config = UILayoutConfig(Partner)
+ui_config.excluded_fields.append('fideninfo')
+
+
+# Настраиваем лейаут
+ui_config.layout = defaultdict(list, {
+    1: ['name', 'type', 'discount'],
+    2: ['phone', 'email'],
+    3: ['director'],
+    4: ['rating']
+})
+
+
+ui_config.set_field_label('name', 'Наименование')
+ui_config.set_field_label('type', 'Тип организации')
+ui_config.set_field_label('discount', 'Скидка')
+ui_config.set_field_label('phone', 'Телефон')
+ui_config.set_special_widget('type', 'combobox', ['ООО', 'ИП', 'АО'])
+ui_config.set_special_widget('phone', 'phone')
+
+
+#Main()
 root = Tk()
-app = DooByApp(root, Partner, title="Учет партнёров", logo_path="resources//logo.png")
+app = DooByApp(root, Partner, title="Учет партнёров", logo_path="resources//logo.png", ui_config=ui_config)
 root.mainloop()
