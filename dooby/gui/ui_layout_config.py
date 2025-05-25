3ff55ec3d2355faf05d078051d116db3b24d836a
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-25 21:00:39 +0300

    Update ui_layout_config.py
    Add suffix functionality

--- dooby/gui/ui_layout_config.py
+++ dooby/gui/ui_layout_config.py
@@ -6,11 +6,11 @@ class UILayoutConfig:
     def __init__(self, model_class):
         self.model_class = model_class
         self.excluded_fields = ['id', 'hideninfo']
-        self.field_labels = {}
+        self.field_labels = {}     # подписи до значения
+        self.field_suffix = {}     # **новое**: текст после значения
         self.special_widgets = {}
         self.layout = defaultdict(list)
         
-        # Автогенерация базового лейаута
         inspector = inspect(model_class)
         fields = [c.name for c in inspector.mapper.columns if c.name not in self.excluded_fields]
         for i, field in enumerate(fields, 1):
@@ -20,7 +20,12 @@ class UILayoutConfig:
         self.layout[row_number].extend(fields)
         
     def set_field_label(self, field: str, label: str):
+        """Текст перед значением"""
         self.field_labels[field] = label
+        
+    def set_field_suffix(self, field: str, suffix: str):
+        """Текст после значения"""
+        self.field_suffix[field] = suffix
         
     def set_special_widget(self, field: str, widget_type: str, options: list = None):
         self.special_widgets[field] = (widget_type, options)
