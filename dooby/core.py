4ea8cc71c80bb69ce0174ea86c58805d389cf577
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-27 13:11:29 +0300

    Update core.py

--- dooby/core.py
+++ dooby/core.py
@@ -1,16 +1,32 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+import importlib
+from dooby.model import *
 
-Base = declarative_base()
+Base = get_base()
 _engine = None
 Session = None
 
-def init_db(path=":memory:"):
+def import_model_module(module_name: str):
+    """
+    Импортирует конкретный модуль, содержащий модели.
+    Например, 'dooby.model'.
+    """
+    importlib.import_module(module_name)
+
+def init_db(path=":memory:", echo=False, model_module="dooby.model"):
+    """
+    Инициализация базы данных.
+    model_module — путь к файлу, в котором находятся все SQLAlchemy модели.
+    """
     global _engine, Session
     _engine = create_engine(f"sqlite:///{path}", echo=False)
     Session = sessionmaker(bind=_engine)
 
-    #Создание всех таблиц моделей при необходимости
+    # Импортируем модели из файла
+    import_model_module(model_module)
+
+    # Создаём таблицы, если они ещё не созданы
     Base.metadata.create_all(_engine)
 
     return _engine
