fc3fe4025796f2e1fcd3cebd5676ef79498de96e
orion787 <116973576+orion787@users.noreply.github.com>
2025-05-27 17:40:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,23 @@ from models import Partner
 
 #Инициализируем БД
 engine = init_db("partners.db")
+
+#Иницилиазция лейаута для UI
 ui_config = UILayoutConfig(Partner)
-ui_config.excluded_fields.append('fideninfo')
+
+#Отказываемся от отображения поля hideninfo
+ui_config.excluded_fields.append('hideninfo')
 
 # Настраиваем лейаут
-ui_config.set_background_color("#F0F0F0")
-ui_config.set_card_background(Colors.lightblue)
-ui_config.set_alignment('left_custom_spacing')
-ui_config.set_inter_element_spacing(40)
-ui_config.set_card_spacing(10)
-ui_config.set_font(('Arial', 14))
+ui_config.set_background_color(Colors.smoke)       #Цвет окна
+ui_config.set_card_background(Colors.lightblue)    #Цвет карточки
+ui_config.set_alignment('left_custom_spacing')     #Выравние в пределах картчоки (доступны: left, center, right, left_custom_spacing)
+ui_config.set_inter_element_spacing(40)            #Отступ между полями для left_custom_spacing
+ui_config.set_card_spacing(10)                     #Отступ между карточками
+ui_config.set_font(('Arial', 12))                  #Выбор шрифта и кегеля
 
 
+#Настройка построчного отображения полей
 ui_config.layout = defaultdict(list, {
     1: ['type', 'name', 'discount'],
     2: ['phone'],
@@ -28,13 +33,16 @@ ui_config.layout = defaultdict(list, {
 })
 
 
-ui_config.set_field_label('name', 'Наименование')
-ui_config.set_field_label('type', 'Тип организации')
-ui_config.set_field_label('discount', 'Скидка')
-ui_config.set_field_suffix('discount', '%') 
-ui_config.set_field_label('phone', 'Телефон')
-ui_config.set_special_widget('type', 'combobox', ['ООО', 'ИП', 'АО'])
+#Настройка отображенияя текста до и после поля, а также в режиме добавления
+ui_config.set_field_label('name', 'Наименование :')                             #Перед полем 
+ui_config.set_field_label('type', 'Тип организации :')                          
+ui_config.set_field_label('discount', 'Скидка :')                               
+ui_config.set_field_suffix('discount', '%')                                     #После поля
+ui_config.set_field_label('phone', 'Телефон :')
+ui_config.set_special_widget('type', 'combobox', ['ООО', 'ИП', 'АО'])           #Выбор для режима добавления
 ui_config.set_special_widget('phone', 'phone')
+ui_config.set_field_label('director', 'Директор :')
+ui_config.set_field_label('rating', 'Рейтинг :')
 
 
 #Main()
